0169c8dcbdd4e61c12b7927234e0c38a5219a598
Sahil Fulmali <sfulmali2907n@gmail.com>
2024-11-11 09:47:24 +0530

    about me responsive done 11/11

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,15 +1,15 @@
 // src/components/AboutMe.jsx
 import styles from "./AboutMe.module.css";
 import React from "react";
-import "./Home.css";
+import "./about.css";
 
 const AboutMe = () => {
   return (
     <section className="about" id="AboutME">
-      <div className="about-container">
+      <div className={styles.about_container}>
         <h1>About Me</h1>
-        <div className="internal-container">
-          <div className="about-me">
+        <div className={styles.internal_container}>
+          <div className={styles.about_me}>
             <p>
               Hi, I'm Sahil Fulmali, a final-year Computer Science student at
               GCOEA Amravati.With a strong foundation in computer science
@@ -23,7 +23,7 @@ const AboutMe = () => {
             </p>
             
           </div>
-          <div className="links">
+          <div className={styles.links}>
             <h1>LINKS</h1>
             <a href="https://github.com/sahilfulmali2" target="_blank">
               <img src="/assets/github_5968866.png" alt="GitHub Profile" />
